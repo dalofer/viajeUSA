bfbe7a42927b5606b0f00404560ce46309eca521
dalofer <dalofer.ciber@gmail.com>
2025-03-09 11:09:01 +0100

    subir login

---
+++ login.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", async function(event) {
+    event.preventDefault(); // Evitar recargar la página
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("https://pregnant-celestina-diegoalonso-0cb5d237.koyeb.app/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        alert("Login exitoso!");
+        localStorage.setItem("token", data.access_token); // Guardar el token en el navegador
+        window.location.href = "dashboard.html"; // Redirigir a otra página
+    } else {
+        alert("Error: " + data.detail);
+    }
+});
